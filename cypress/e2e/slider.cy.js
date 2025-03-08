e07a3212bf246d130740544317d8f91c28312368
Dante734 <piotre.karkoszka@gmail.com>
2025-03-08 15:54:41 +0100

    Update slider.cy.js

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,16 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Navigation Test', function () {
+  it('Should navigate through slides using "next" and "prev" buttons', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Germany');
+    cy.get('.swiper-button-next').click();  // Kliknięcie przycisku "Następny"
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom'); // Sprawdzenie, czy aktywny slajd zawiera "United Kingdom"
+
+    // Krok 3: Kliknięcie przycisku "Poprzedni slajd"
+    cy.get('.swiper-button-prev').click();  // Kliknięcie przycisku "Poprzedni"
+    cy.get('.swiper-slide-active').should('contain', 'Germany'); // Sprawdzenie, czy powróciliśmy do "Germany"
+  });
+});
