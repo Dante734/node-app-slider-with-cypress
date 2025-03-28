658bf160cf544667a9d326af51eb256a99909149
Dante734 <piotre.karkoszka@gmail.com>
2025-03-28 18:46:14 +0100

    dodano hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -17,15 +17,26 @@ describe('Swiper Gallery Test', function () {
   });
 });
 
-describe('Swiper Gallery Navigation Test', function () {
-  it('Should navigate through slides using "next" and "prev" buttons', function () {
-    cy.visit('http://localhost:3000');
-    cy.get('.swiper-slide-active').should('contain', 'Germany');
-    cy.get('.swiper-button-next').click();  // Kliknięcie przycisku "Następny"
-    cy.get('.swiper-slide-active').should('contain', 'United Kingdom'); // Sprawdzenie, czy aktywny slajd zawiera "United Kingdom"
+describe('Swiper Gallery Description Test', function () {
+  const slides = [
+    { title: 'Rome', description: 'Italy' },
+    { title: 'United Kingdom', description: 'London' },
+    { title: 'Paris', description: 'France' }
+  ];
 
-    // Krok 3: Kliknięcie przycisku "Poprzedni slajd"
-    cy.get('.swiper-button-prev').click();  // Kliknięcie przycisku "Poprzedni"
-    cy.get('.swiper-slide-active').should('contain', 'Germany'); // Sprawdzenie, czy powróciliśmy do "Germany"
+  it('Checks if each slide displays the correct title and description', function () {
+    // Krok 1: Odwiedzenie strony galerii
+    cy.visit('http://localhost:3000');
+
+    slides.forEach((slide, index) => {
+      if (index > 0) {
+        cy.get('.swiper-button-next').click(); // Kliknięcie "Następny slajd" dla kolejnych slajdów
+        cy.wait(1000); // Krótkie oczekiwanie na przejście slajdu
+      }
+
+      // Krok 2: Sprawdzenie, czy tytuł i opis są widoczne
+      cy.get('.swiper-slide-active').should('contain', slide.title);
+      cy.get('.swiper-slide-active').should('contain', slide.description);
+    });
   });
 });
